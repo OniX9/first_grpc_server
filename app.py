946ec940fe83b1659bc4cfe13b2e10d4f11bf930
OniX9 <onisemem9@gmail.com>
2026-08-02 17:35:41 +0100

    added listen for port env

--- app.py
+++ app.py
@@ -1,18 +1,6 @@
-# Copyright 2015 gRPC authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 """The Python implementation of the GRPC users.Users server."""
 
+import os
 import logging
 from concurrent import futures
 
@@ -23,10 +11,11 @@ from src.resources.users import Users
 
 
 def serve():
-    port = "50051"
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=100))
+    port = os.getenv("PORT", "50051")
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     users_pb2_grpc.add_UsersServicer_to_server(Users(), server)
-    server.add_insecure_port("[::]:" + port)
+    # server.add_insecure_port("[::]:" + port)
+    server.add_insecure_port(f"0.0.0.0:{port}")
     server.start()
     print("Server started, listening on " + port)
     server.wait_for_termination()
